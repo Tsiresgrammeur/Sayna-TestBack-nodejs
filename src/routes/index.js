087fe4043ecc96a747f8f3597faeb42953b10a19
Tsiresgrammeur <tsiresgrammeur@gmail.com>
2021-12-25 18:57:11 +0300

    adjusting route

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cartController = require('../controllers/cart.c');
+const resourceController = require('../controllers/resources.c');
+const userController = require('../controllers/user.c');
+
+const router = express.Router();
+
+router.post('/login', userController.login)
+router.post('/register', userController.register);
+router.put('/subscription',cartController.subscribe)
+//router.put('/user', userController.update);
+//router.delete('/user', userController.delete);
+//router.delete('/user/off', userController.logOut);
+router.put('/user/cart', cartController.update);
+//router.get('/songs',siteController.searchSite);
+//router.get('/songs/:id',siteController.searchSite);
+
+module.exports = router;
