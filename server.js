4744419bcf6bffcb110901d700e35679261b66ca
Tsiresgrammeur <tsiresgrammeur@gmail.com>
2021-12-26 16:44:46 +0300

    get a specific song

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.get('/', (req, res) => {
 });
 
 app.get('/songs', async function(req, res) {
-  console.log('fjldks');
   try
   {
     var decoded = jwt_decode(req.headers.authorization);
@@ -56,7 +55,50 @@ app.get('/songs', async function(req, res) {
       res.status(403).send({ error: true, "message":"Votre abonnement ne permet pas d'accéder à la ressource" })
     }
 
-  console.log('dlf')
+});
+
+app.get('/songs/:id', async function(req, res) {
+  try
+  {
+    var decoded = jwt_decode(req.headers.authorization);
+  }
+  catch(error)
+  {
+    res.status(401).send({ error: true, message: 'Token n\'est pas correct' });
+    return;
+  }
+  var songGot,songs=[];
+  if(decoded.subscription == "PREMIUM")
+  {
+    const snapshot = await firebase.collection('song').get();
+    snapshot.forEach((doc) => {
+      if(req.params.id == doc.id)
+      {
+          let createdAt=doc.data().createdAt;
+          let dateInMillis = createdAt._seconds * 1000;
+          var dateCreate= new Date(dateInMillis).toDateString() + ' at ' + new Date(dateInMillis).toLocaleTimeString();
+          let updatedAt=doc.data().updatedAt;
+          let dateInMillis2 = updatedAt._seconds * 1000;
+          var dateUpdate= new Date(dateInMillis2).toDateString() + ' at ' + new Date(dateInMillis2).toLocaleTimeString();
+          songGot= doc.data();
+          songGot.createdAt=dateCreate;
+          songGot.updatedAt=dateUpdate;
+          songGot.id=doc.id;
+          songs.push(songGot);
+          res.status(200).send({ 
+            error: false, 
+            songs
+          })
+      }
+
+    });
+  }
+  else
+  {
+    res.status(403).send({ error: true, "message":"Votre abonnement ne permet pas d'accéder à la ressource" })
+  }
+
+
 });
 
 //app.get('/**', (req, res) => {
